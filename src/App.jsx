9ac8dc1ccb68313f69c7a36dbb8771425cd9761f
gerardnicolas <77805536+gerardnicolas@users.noreply.github.com>
2024-01-27 03:16:18 +0800

    update error handling and added footer

--- src/App.jsx
+++ src/App.jsx
@@ -2,79 +2,77 @@ import axios from "axios";
 import { useRef, useState } from "react"
 import { youtube_parser } from "./utils";
 import Header from "./components/Header";
+import Footer from "./components/Footer"
 
 function App() {
   const inputUrlRef = useRef();
   const [urlResult, setUrlResult] = useState(null);
   const [errorDisplay, setErrorDisplay] = useState("")
+  const [songTitle, setSongTitle] = useState("")
+  
+  function handleSubmit(event){
+    event.preventDefault()
+    const youtubeId = youtube_parser(inputUrlRef.current.value)
 
-  const handleSubmit = (e) => {
-    e.preventDefault()
-    const youtubeID = youtube_parser(inputUrlRef.current.value);
-
-    const options = {
+    axios ({
       method: 'GET',
       url: 'https://youtube-mp36.p.rapidapi.com/dl',
       params: {
-        id: youtubeID
+        id: youtubeId
       },
       headers: {
         'X-RapidAPI-Key': import.meta.env.VITE_RAPID_API_KEY,
         'X-RapidAPI-Host': 'youtube-mp36.p.rapidapi.com'
-      }
-    }
-
-
-    axios(options)
-      .then((res) => {
-        if (res.data.link) {
-          setUrlResult(res.data.link)
-        } else {
-          console.log(res.data.msg) 
-          setErrorDisplay("Something went wrong. Enter a valid youtube link.")
         }
-      }) 
-      .catch ((error) => {
-        if (error.data) { // status code out of the range of 2xx
-          console.log("Data :" , error.data);
-          console.log("Status :" + error.data.status);
-        } else if (error.request) { // The request was made but no response was received
-          console.log(error.request);
-        } else {// Error on setting up the request
-          console.log('Error', error.data.msg);
+    })
+      .then((res) => {
+        if (res.data.link && res.data.title){
+          setUrlResult(res.data.link)
+          setSongTitle(res.data.title)
+          setErrorDisplay("")
+          console.log("Successfully fetched data!")
+        } else if (!res.data.link){
+          console.log(`Error: ${res.data.msg}`)
+          setErrorDisplay("Enter a valid youtube link.")
+          setSongTitle("")
+          setUrlResult("")
         }
       })
-      // .catch (error => console.log(error))
     inputUrlRef.current.value = '';
   }
 
   return (
     <div>
-    <Header />
-    <div className="flex-col justify-center items-center pt-44">
-      <div className="">
-              <h1 className='flex font-karla justify-center font-bold lg:text-4xl md:text-3xl sm:text-3xl pb-3 text-2xl'>Youtube to MP3 Converter</h1>
-              <p className="flex font-karla justify-center text-s pb-10">Convert a Youtube Video to MP3!</p>
-          </div>
-          
-          <form onSubmit={handleSubmit} className='flex font-karla justify-center p-2 mx-16'>
-              <input ref={inputUrlRef} className='bg-slate-300 p-2 w-80' 
-              type="text" id="ytId" placeholder='Paste Youtube link here' autoComplete="off"/>
-              <button className='bg-slate-300 p-2 ml-2 rounded hover:bg-slate-200 transition-all' type='submit' id='convert-button'>Convert</button>
-          </form>
+      <Header />
+      <div className="flex-col justify-center items-center pt-52">
+        <div className="">
+          <h1 className='flex font-karla justify-center font-bold lg:text-4xl md:text-3xl sm:text-3xl pb-3 text-2xl'>Youtube to MP3 Converter</h1>
+          <p className="flex font-karla justify-center text-s pb-10">Convert a Youtube Video to MP3!</p>
+        </div>
+            
+        <form onSubmit={handleSubmit} className='flex font-karla justify-center p-2 mx-16'>
+          <input ref={inputUrlRef} className='bg-slate-300 p-2 w-80' 
+          type="text" id="ytId" placeholder='Paste Youtube link here' autoComplete="off"/>
+          <button className='bg-slate-300 p-2 ml-2 rounded hover:bg-slate-200 transition-all' type='submit' id='convert-button'>Convert</button>
+        </form>
 
-          <div className="flex justify-center mt-10">
-            {errorDisplay ? <h1>{errorDisplay}</h1> : ""}
-          </div>
+        <div className="flex justify-center mt-14 font-karla text-lg font-bold">
+          {songTitle ? <h1>Title: {songTitle}</h1> : ""}
+          {}
+        </div>
 
-          <div className="flex justify-center mt-10">
-            {urlResult ? <a target='_blank' rel="noreferrer" href={urlResult}
-            className="font-karla bg-blue-500 text-white p-2 
-            rounded font-semibold transition-color duration-200 hover:bg-blue-400">Download MP3</a> : ''}
-          </div>
+        <div className="flex justify-center mt-1 font-karla text-red-500">
+          {errorDisplay ? <h1>{errorDisplay}</h1> : ""}
+        </div>
+
+        <div className="flex justify-center mt-1">
+          {urlResult ? <a target='_blank' rel="noreferrer" href={urlResult}
+          className="font-karla bg-blue-500 text-white p-2 
+          rounded font-semibold transition-color duration-200 hover:bg-blue-400">Download MP3</a> : ''}
+        </div>
       </div>
+      <Footer />
     </div>
-        
   )
 }
 
---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div className="fixed left-0 bottom-0 w-full p-4 text-white bg-gray-900 flex justify-center">
+        <p>Created using React and TailwindCSS.</p>
+    </div>
+  )
+}
+
+export default Footer
